ac162c5875c06abffdae66e89c7249c3f174ffd8
Bryce <bryceewatson@gmail.com>
2015-06-16 20:23:13 -0700

    adding server + fixing map

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path    = require("path");
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/fonts',express.static(__dirname + '/fonts'));
+app.use('/images',express.static(__dirname + '/images'));
+app.use('/js',express.static(__dirname + '/js'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+var server = app.listen(8080, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
